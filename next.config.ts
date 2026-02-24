1d9a8d78caebf89a7fa59728ffda43791a990ad4
TylorShine <ricka.tylor@gmail.com>
2026-02-24 21:33:15 +0900

    update: add .nojekyll for deploy to pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,24 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = 'prompt-morpher';
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repository = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || repo;
+  assetPrefix = `/${repository}/`;
+  basePath = `/${repository}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/prompt-morpher",
-  assetPrefix: "/prompt-morpher",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
