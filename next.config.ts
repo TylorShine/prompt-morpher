283ebf7aa7063bc5890d22b1e3949f422cacff3c
TylorShine <ricka.tylor@gmail.com>
2026-02-24 18:48:07 +0900

    fix: add basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/prompt-morpher",
+  assetPrefix: "/prompt-morpher",
 };
 
 export default nextConfig;
